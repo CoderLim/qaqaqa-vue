ba2f04f8ad28727eaceeda63e2d914f82fa0e5e9
耿笠茗 <gengliming@bytedance.com>
2019-03-25 10:58:59 +0800

    yyy

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 
+## Prerequisite
+
+1. Install vue-cli;
+2. yarn global add @vue/cli-service-global
+
+## Run
+
+vue serve {file}
+
 ## 一些关于 vue 的小疑问
 
 ### 计算属性不能用箭头函数
@@ -48,6 +57,18 @@ US —> [信息流us]
 初始设置 region为us，group为[信息流]，group肯定展示的是空，如果修改region为cn，group也不会更新展示
 
 ### 29、记一次状态管理的坑
+现象：点击时间轴后，分组select变成了all
+
+1. 查看groupsInSelectedRegion(computed)，依赖与groups和formData.region；
+2. 使用watch看到：groups有变化和formData.region无变化；
+3. groups是直接保存在vuex的store里的，通过接口获取的数据，经查看内容发现groups并没有修改；
+4. groups内容没变，难道groups被clone重新赋值的？
+5. 通过查看vue的devtool发现，点击时间轴后，触发了两次Action：MOUNTED
+6. 经多打断点发现是Jimu里的action，难道是积木修改了vuex的store？打印store.state发现确实多了一个jimu这个变量；
+7. 但是通过代码没发现clone数据再赋值的，但是发现jimu这个变量是$set；
+8. 试一下直接temp.$set(temp.$store.state, 'new1', 'value1'), 确实state下的变量都被通知了；
+9. 然后试了一下普通变量temp.$set(temp.formData.test, 'new1', 'value1'), 发现formData的子数据没被通知，那么问题就是vuex了，当添加新属性时，会通知所有子数据；
+10. 但是看过vuex的代码后没发现有这逻辑，经过几次尝试，发现添加新属性时，只有非基本数据类型的变量才会被通知，wtf！
 
 ### 29、计算属性的循环引用
 
--- src/changing-one-field-will-trigger-host-deps-called.vue
+++ src/changing-one-field-will-trigger-host-deps-called.vue
@@ -1,14 +1,25 @@
 <!--
-  只修改name，不会触发host的依赖执行回调
-  如果修改host，则name和addr直接与原来的值做比较，如果一致，则不触发依赖的回调函数
-  如果是给host新增field，则host的watch也会执行；
+  1、只修改name，不会触发host的依赖执行回调
+  2、如果修改host，则name和addr直接与原来的值做比较，如果一致，则不触发依赖的回调函数
+  3、如果是给host新增field，host的Dep回调也会执行；
+	                       host下的基本数据类型不会触发依赖回调；
+												 host下的复杂类型也会触发依赖回调；
+		 IMPORTANT: 如果想测试这种情况，必须保证host内的数据在template中都用到了，
+		 因为vue的动态依赖收集策略，如果没用到就不存在dep回调触发的逻辑
+												 
   如果是给vuex的$store.state设置新属性，那state下的所有属性都会被通知
+	这个是幻觉，请参考上面的第3点;
+
+	ps: 新增field指的是通过$set方法
 -->
 <template>
   <div>
     {{host.name}}
     <br>
     {{host.addr}}
+		<ul>
+			<li v-for="t of host.childList" :key="t">{{ t }}</li>
+		</ul>
     <button @click="btnOnClick">change host</button>
     <button @click="changeNameBtnOnClick">change host.name</button>
     <button @click="addNewFieldOnClick">add new field to `host</button>
@@ -23,10 +34,7 @@ export default {
       host: {
         name: 'host_name',
         addr: 'host_addr',
-        childHost: {
-          field1: 'value1',
-          field2: 'value1',
-        },
+        childList: [ 1, 2, 3,4,5, ],
       },
     };
   },
@@ -54,8 +62,8 @@ export default {
     'host.name'() {
       console.log('host.name changed');
     },
-    'host.newField'() {
-      console.log('newField changed');
+    'host.childList'() {
+      console.log('complicated child notified');
     },
   },
   mounted() {
